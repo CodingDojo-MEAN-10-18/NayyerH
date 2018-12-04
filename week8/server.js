fcb97ff4497206271d0cfbf5934073487a979c3d
NMH37 <36855337+NMH37@users.noreply.github.com>
2018-12-03 22:56:02 -0600

    server

---
+++ week8/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.resolve('dist/public'))); // production use
+
+// add database config here
+require('./server/config/database');
+
+app.use('/api',require('./server/routes'));
+app.use(require('./server/routes/catch-all.route'));
+app.listen(port,()=> console.log(`listening on port ${port}`))
